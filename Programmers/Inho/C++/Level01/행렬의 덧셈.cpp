37e3405d4a27d82f9ddab520eb01714f244486d5
jeontigger <boy9609@naver.com>
2022-03-24 18:07:43 +0900

    add: 행렬의 덧셈

---
+++ Programmers/Inho/C++/Level01/행렬의 덧셈.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer;
+    vector<int> temp;
+    for(int i = 0; i<arr1.size() ; i++){
+        temp.clear();
+        for(int j = 0; j<arr1[0].size(); j++){
+            temp.push_back(arr1[i][j] + arr2[i][j]);
+        }
+        answer.push_back(temp);
+    }
+    cout << arr1[0].size() << endl;
+    return answer;
+}
