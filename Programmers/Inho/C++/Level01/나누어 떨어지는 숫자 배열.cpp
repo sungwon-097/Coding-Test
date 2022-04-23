a9364d9e1d138fdcfb69a352d5e16cf150d3f626
jeoninho <boy9609@naver.com>
2022-04-23 16:43:32 +0900

    add: 나누어 떨어지는 숫자 배열

---
+++ Programmers/Inho/C++/Level01/나누어 떨어지는 숫자 배열.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    for(int num:arr){
+        if(num%divisor==0)
+            answer.push_back(num);
+    }
+    if(answer.size()==0)
+        answer.push_back(-1);
+    sort(answer.begin(), answer.end());
+    return answer;
+}
